6cdc319604d5869ee5f75d4e612faf349b51e765
milosdjurica <milosdjurica13@gmail.com>
2023-11-16 18:36:48 +0100

    registering new user done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,39 @@ import { DatabaseService } from "@Src/database/database.service";
 import { Injectable } from "@nestjs/common";
 import { LogInDto, RegisterDto } from "./dto";
 import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
+import { Tokens } from "./types";
+import { UsersService } from "@Src/users/users.service";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
 
-  // TODO add return types Promise<Token>
-  async registerLocal(registerDto: RegisterDto) {
+  // ! This will only be used by normal users
+  // TODO In user service i will create a method for admin to add other users or other admins
+  async registerLocal(registerDto: RegisterDto): Promise<Tokens> {
+    // TODO if user with that username exist, throw error to pick another username
+    // const userExist = await this.userService.findOne(registerDto.username);
+
     const hash = await this.hashData(registerDto.password);
-    const newUser = this.databaseService.user.create({
+    const newUser = await this.databaseService.user.create({
       data: {
         username: registerDto.username,
         password: hash,
       },
     });
-    return newUser;
+
+    const tokens = await this.getTokens(
+      newUser.id,
+      newUser.username,
+      newUser.role,
+    );
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
 
   async logInLocal(logInDto: LogInDto) {
@@ -26,6 +44,57 @@ export class AuthService {
   async logOutLocal() {}
 
   async refreshTokens() {}
+
+  async updateRtHash(id: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.databaseService.user.update({
+      where: {
+        id,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
+  async getTokens(
+    userId: number,
+    username: string,
+    // TODO fix type
+    role: "USER" | "ADMIN",
+  ): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+          role,
+        },
+        {
+          secret: process.env.AT_SECRET,
+          // 3h
+          expiresIn: 60 * 60 * 3,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+          role,
+        },
+        {
+          secret: process.env.RT_SECRET,
+          // 7days
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.AT_SECRET,
+    });
+  }
+  // TODO properly put type to this instead of any
+  validate(payload: any) {
+    // req.user = payload;
+    return payload;
+  }
+}
