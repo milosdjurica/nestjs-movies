cda630247a05721eef4a2c3796480fcbf91563c3
milosdjurica <milosdjurica13@gmail.com>
2023-11-19 19:01:31 +0100

    series-genre service dont

--- src/series-genre/series-genre.service.ts
+++ src/series-genre/series-genre.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { DatabaseService } from "@Src/database/database.service";
+import { CreateGenreDto } from "@Src/genres/dto";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class SeriesGenreService {}
+export class SeriesGenreService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async deleteAndCreateNewSeriesGenre(
+    seriesId: number,
+    genres: CreateGenreDto[],
+  ) {
+    await this.deleteSeriesGenres(seriesId);
+    const seriesGenresCreateData =
+      await this.createOrConnectGenresWithSeries(genres);
+
+    await this.databaseService.series.update({
+      where: { id: seriesId },
+      data: {
+        seriesGenres: { create: seriesGenresCreateData },
+      },
+    });
+  }
+
+  async createOrConnectGenresWithSeries(genres: CreateGenreDto[]) {
+    return Promise.all(
+      genres.map(async (genre) => {
+        const createdGenre = await this.databaseService.genre.upsert({
+          where: { name: genre.name },
+          update: {},
+          create: genre,
+        });
+        return { genre: { connect: { id: createdGenre.id } } };
+      }),
+    );
+  }
+
+  async deleteSeriesGenres(seriesId: number) {
+    await this.databaseService.seriesGenre.deleteMany({
+      where: { seriesId },
+    });
+  }
+}
