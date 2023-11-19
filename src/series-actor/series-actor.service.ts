7bd8b6909e365eee4c6d49f78286069c8f72c7d3
milosdjurica <milosdjurica13@gmail.com>
2023-11-19 18:59:40 +0100

    series-actor service

--- src/series-actor/series-actor.service.ts
+++ src/series-actor/series-actor.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { CreateActorDto } from "@Src/actors/dto";
+import { DatabaseService } from "@Src/database/database.service";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class SeriesActorService {}
+export class SeriesActorService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async deleteAndCreateNewSeriesActor(
+    seriesId: number,
+    actors: CreateActorDto[],
+  ) {
+    await this.deleteSeriesActors(seriesId);
+    const seriesActorsCreateData =
+      await this.createOrConnectActorsWithSeries(actors);
+
+    await this.databaseService.series.update({
+      where: { id: seriesId },
+      data: {
+        seriesActors: { create: seriesActorsCreateData },
+      },
+    });
+  }
+
+  async createOrConnectActorsWithSeries(actors: CreateActorDto[]) {
+    return Promise.all(
+      actors.map(async (actor) => {
+        const createdActor = await this.databaseService.actor.upsert({
+          where: { name: actor.name },
+          update: {},
+          create: actor,
+        });
+        return { actor: { connect: { id: createdActor.id } } };
+      }),
+    );
+  }
+
+  async deleteSeriesActors(seriesId: number) {
+    await this.databaseService.seriesActor.deleteMany({
+      where: { seriesId },
+    });
+  }
+}
